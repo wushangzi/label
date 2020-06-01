43878b51ffce635543219748ca8f17cd4b502d75
chengyongjun <2607707289@qq.com>
2020-06-01 01:17:48 -0400

    add label status

--- src/main/java/common/GlobalPara.java
+++ src/main/java/common/GlobalPara.java
@@ -11,5 +11,12 @@ public class GlobalPara {
     public static final String INIT_LABEL_NAME = "label_info";
 
     public static final String INIT_FORMAT_LABEL_STRING = "create table if not exists %s." + INIT_LABEL_NAME +
-            " (id int primary key auto_increment,name varchar(40) not null,bit_index smallint NOT NULL unique)";
+            " (id int primary key auto_increment,schema_name varchar(40),table_name varchar(40),name varchar(40)" +
+            " not null,bit_index smallint NOT NULL default 1,bit_index_status smallint not null default 0," +
+            "UNIQUE KEY `schema_table_bit_unique` (`schema_name`,`table_name`,`bit_index`))";
+
+
+    public static final int LABEL_STATUS_NORMAL = 1;
+
+    public static final int LABEL_STATUS_DELETED = 1 << 1;
 }
